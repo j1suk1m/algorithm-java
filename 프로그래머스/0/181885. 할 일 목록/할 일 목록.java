3ccc3bb201599dfbc2d28d24b5d0839a2ca06425
Jisu Kim <jisukim.tech@gmail.com>
2024-11-21 08:48:46 +0900

    [level 0] Title: 할 일 목록, Time: 0.07 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        ArrayList<String> answer = new ArrayList<>();
+        
+        for (int idx = 0; idx < todo_list.length; idx++) {
+            if (!finished[idx]) {
+                answer.add(todo_list[idx]);
+            }
+        }
+        
+        return answer.toArray(new String[0]);
+    }
+}
