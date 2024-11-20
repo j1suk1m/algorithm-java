1102ccf64519ad3d37473836ab7c7714dd6d66ff
Jisu Kim <jisukim.tech@gmail.com>
2024-11-21 04:40:16 +0900

    [level 0] Title: A 강조하기, Time: 0.04 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,17 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        StringBuilder sb = new StringBuilder();
-        
-        for (char alphabet : myString.toCharArray()) {
-            if (alphabet == 'a') {
-                sb.append("A");
-            } else if (alphabet != 'A' && Character.isUpperCase(alphabet)) {
-                sb.append(Character.toLowerCase(alphabet));
-            } else {
-                sb.append(alphabet);
-            }
-        }
-        
-        return sb.toString();
+        return myString.toLowerCase().replace("a", "A");
     }
 }
