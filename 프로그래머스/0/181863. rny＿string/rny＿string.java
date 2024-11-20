b162df0ccf6e75b27f4180a397d5f9d3d1a0665e
Jisu Kim <jisukim.tech@gmail.com>
2024-11-21 03:37:44 +0900

    [level 0] Title: rny_string, Time: 0.06 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.java
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -1,16 +1,5 @@
 class Solution {
     public String solution(String rny_string) {
-        StringBuilder sb = new StringBuilder();
-        char[] array = rny_string.toCharArray();
-        
-        for (char alphabet : array) {
-            if (alphabet == 'm') {
-                sb.append("rn");
-            } else {
-                sb.append(alphabet);
-            }
-        }
-        
-        return sb.toString();
+        return rny_string.replace("m", "rn");
     }
 }
