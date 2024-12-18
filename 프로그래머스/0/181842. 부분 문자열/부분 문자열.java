1ff2a0f17d35c1dd4442cb14fab7ff9dd071fc60
JisuKim <jisukim.tech@gmail.com>
2024-12-19 07:57:33 +0900

    [level 0] Title: 부분 문자열, Time: 0.03 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
@@ -1,16 +1,5 @@
 class Solution {
     public int solution(String str1, String str2) {
-        int answer = 0;
-        
-        for (int i = 0; i <= str2.length() - str1.length(); i++) {
-            if (str1.charAt(0) == str2.charAt(i)) {
-                if (str1.equals(str2.substring(i, i + str1.length()))) {
-                    answer = 1;
-                    break;
-                }
-            }
-        }
-        
-        return answer;
+        return str2.contains(str1) ? 1 : 0;
     }
 }
