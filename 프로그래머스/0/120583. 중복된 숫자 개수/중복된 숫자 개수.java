5e5a3c26cda8f5329a0dc540ef0f4ddb38630d34
JisuKim <jisukim.tech@gmail.com>
2024-12-20 23:01:33 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.03 ms, Memory: 88.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        
+        for (int number : array) {
+            if (number == n) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
