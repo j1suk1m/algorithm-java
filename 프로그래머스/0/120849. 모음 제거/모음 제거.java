f27786865a1da5fe0e2372daddeadf6ef2ca66a1
JisuKim <jisukim.tech@gmail.com>
2024-12-20 22:56:00 +0900

    [level 0] Title: 모음 제거, Time: 0.12 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,5 +1,7 @@
 class Solution {
     public String solution(String my_string) {
-        return my_string.replace("a", "").replace("e", "").replace("i", "").replace("o", "").replace("u", "");
+        String regex = "[aeiou]";
+        
+        return my_string.replaceAll(regex, "");
     }
 }
