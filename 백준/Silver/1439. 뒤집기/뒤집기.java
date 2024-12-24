d27e28628aa2293f5a996ab5d86562e36114e494
JisuKim <jisukim.tech@gmail.com>
2024-12-24 15:46:37 +0900

    [Silver V] Title: 뒤집기, Time: 124 ms, Memory: 15120 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -1,19 +1,17 @@
 import java.io.*;
+import java.util.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
-        int[] counts = new int[2]; // (문자열 압축 후) 0의 개수, 1의 개수
+        int[] counts = new int[2]; // 연속된 0 묶음의 개수, 연속된 1 묶음의 개수
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        
-        String input = reader.readLine();
-        
-        // 한 번 이상 연속으로 등장하는 숫자를 하나로 압축
-        String compressedInput = input.replaceAll("0+", "0").replaceAll("1+", "1");
 
-        for (int i = 0; i < compressedInput.length(); i++) {
-            counts[Character.getNumericValue(compressedInput.charAt(i))]++;
-        }
-        
+        String input = reader.readLine();
+
+        // 연속된 묶음의 개수 저장
+        counts[0] = (int) Arrays.stream(input.split("1+")).filter(s -> !s.isEmpty()).count();
+        counts[1] = (int) Arrays.stream(input.split("0+")).filter(s -> !s.isEmpty()).count();
+
         System.out.println(Math.min(counts[0], counts[1]));
     }
 }
