aec6974791d9c049a1c6f007fdd76cd9c3599af7
JisuKim <jisukim.tech@gmail.com>
2024-12-26 17:23:22 +0900

    [level 2] Title: 전화번호 목록, Time: 270.90 ms, Memory: 213 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,17 +1,17 @@
-import java.util.Set;
-import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
 
 class Solution {
     public boolean solution(String[] phoneBook) {
-        Set<String> set = new HashSet<>();
+        Map<String, Integer> map = new HashMap<>();
         
         for (String phoneNumber : phoneBook) {
-             set.add(phoneNumber);
+             map.put(phoneNumber, 0);
         }
         
         for (String phoneNumber : phoneBook) {
             for (int i = 1; i < phoneNumber.length(); i++) {
-                if (set.contains(phoneNumber.substring(0, i))) {
+                if (map.containsKey(phoneNumber.substring(0, i))) {
                     return false;
                 }
             }
