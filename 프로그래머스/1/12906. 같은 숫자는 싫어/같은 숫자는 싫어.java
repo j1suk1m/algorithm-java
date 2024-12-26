0f5737ed3b85e65f4047334b9328f578a13c9070
JisuKim <jisukim.tech@gmail.com>
2024-12-27 03:37:01 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 54.92 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,24 +1,21 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Stack;
 
 public class Solution {
     public int[] solution(int[] arr) {
-        Deque<Integer> deque = new ArrayDeque<>();
+        Stack<Integer> stack = new Stack<>();
         
         for (int i = 0; i < arr.length; i++) {
             int current = arr[i];
             
-            if (!deque.isEmpty() && deque.peekLast() == current) {
-                deque.offerLast(deque.pollLast());
-            } else {
-                deque.offerLast(current);
+            if (stack.isEmpty() || stack.peek() != current) {
+                stack.push(current);
             }
         }
         
-        int[] answer = new int[deque.size()];
+        int[] answer = new int[stack.size()];
         
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = deque.pollFirst();
+        for (int i = answer.length - 1; i >= 0; i--) {
+            answer[i] = stack.pop();
         }
 
         return answer;
