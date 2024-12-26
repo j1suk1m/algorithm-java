23c8053e0a401b30eb2f3dc1d7d9864e639c7339
JisuKim <jisukim.tech@gmail.com>
2024-12-26 23:38:13 +0900

    [level 3] Title: 베스트앨범, Time: 3.49 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.stream.Collectors;
+import java.util.PriorityQueue;
 
 class Song implements Comparable<Song> {
     private final int id;
@@ -23,17 +23,13 @@ class Song implements Comparable<Song> {
 
     @Override
     public int compareTo(Song other) {
-        if (this.playCount > other.getPlayCount()) {
-            return -1;
-        } else if (this.playCount < other.getPlayCount()) {
-            return 1;
-        } else {
-            if (this.id < other.getId()) {
-                return -1;
-            } else {
-                return 1;
-            }
+        int result = Integer.compare(other.getPlayCount(), this.playCount);
+        
+        if (result != 0) {
+            return result;
         }
+        
+        return Integer.compare(this.id, other.getId());
     }
 }
 
@@ -41,35 +37,30 @@ class Solution {
     public int[] solution(String[] genres, int[] plays) {
         List<Integer> answer = new ArrayList<>();
         Map<String, Integer> genreToPlayCounts = new HashMap<>();
-        Map<String, List<Song>> genreToSongs = new HashMap<>();
+        Map<String, PriorityQueue<Song>> genreToSongs = new HashMap<>();
 
         for (int i = 0; i < genres.length; i++) {
             String genre = genres[i];
             int playCount = plays[i];
-            Song song = new Song(i, playCount);
 
             genreToPlayCounts.put(genre, genreToPlayCounts.getOrDefault(genre, 0) + playCount);
-            List<Song> songs = genreToSongs.getOrDefault(genre, new ArrayList<>());
-            songs.add(song);
+
+            PriorityQueue<Song> songs = genreToSongs.getOrDefault(genre, new PriorityQueue<>());
+            songs.add(new Song(i, playCount));
             genreToSongs.put(genre, songs);
         }
 
-        List<Map.Entry<String, Integer>> sortedGenreToPlayCounts = genreToPlayCounts.entrySet()
-                                                                                    .stream()
-                                                                                    .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
-                                                                                    .collect(Collectors.toList());
-
-        for (Map.Entry<String, List<Song>> entry : genreToSongs.entrySet()) {
-            List<Song> songs = entry.getValue();
-            songs.sort(null);
-        }
+        List<Map.Entry<String, Integer>> sortedGenreToPlayCounts = new ArrayList<>(genreToPlayCounts.entrySet());
+        sortedGenreToPlayCounts.sort((e1, e2) -> e2.getValue().compareTo(e1.getValue()));
 
         for (Map.Entry<String, Integer> entry : sortedGenreToPlayCounts) {
             String genre = entry.getKey();
-            List<Song> songs = genreToSongs.get(genre);
+            PriorityQueue<Song> songs = genreToSongs.get(genre);
+            int size = songs.size();
 
-            for (int i = 0; i < Math.min(2, songs.size()); i++) {
-                answer.add(songs.get(i).getId());
+            for (int i = 0; i < Math.min(2, size); i++) {
+                Song song = songs.poll();
+                answer.add(song.getId());
             }
         }
 
