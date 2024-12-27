e93e736803e1f4ec6fae76f7053e25640c09394f
JisuKim <jisukim.tech@gmail.com>
2024-12-27 20:28:09 +0900

    [level 2] Title: 올바른 괄호, Time: 18.53 ms, Memory: 54.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,28 @@
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);
+            
+            try {
+                if (current == '(') {
+                    stack.push(current);
+                } else {
+                    stack.pop();
+                }
+            } catch (EmptyStackException e) {
+                return false;
+            }
+        }
+        
+        if (stack.empty()) {
+            return true;
+        }
+        
+        return false;
+    }
+}
