c8a9b9aacb8ed72c6f2f73c1f72f2f63c980a9f4
JisuKim <jisukim.tech@gmail.com>
2025-01-03 23:30:58 +0900

    [level 2] Title: 기능개발, Time: 3.75 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -6,16 +6,16 @@ import java.util.Queue;
 class Task {
     private final int progress;
     private final int speed;
-    
+
     Task(int progress, int speed) {
         this.progress = progress;
         this.speed = speed;
     }
-    
+
     public int getProgress() {
         return progress;
     }
-    
+
     public int getSpeed() {
         return speed;
     }
@@ -26,43 +26,47 @@ class Solution {
     private static List<Integer> answer = new ArrayList<>();
 
     public int[] solution(int[] progresses, int[] speeds) {
-        int deploymentOrder = -1;
-        int passedDays = 0;
-        
-        init(progresses, speeds);
-        
+        int deploymentOrder = -1; // 배포 순서 // answer 리스트의 인덱스로 사용
+        int passedDays = 0; // 지난 날짜
+
+        init(progresses, speeds); // 초기화
+
         while (!queue.isEmpty()) {
             Task task = queue.poll();
             int progress = task.getProgress();
             int speed = task.getSpeed();
-            
+
             if (!isDeployable(progress, speed, passedDays)) {
-                deploymentOrder++;
-                passedDays = calculatePassedDays(progress, speed);
+                deploymentOrder++; // 다음 배포 순서에 배포
+                passedDays = calculatePassedDays(progress, speed); // 배포 가능한 날짜 계산
             }
-            
-            deploy(deploymentOrder);
+
+            deploy(deploymentOrder); // 배포 
         }
-        
+
         return answer.stream().mapToInt(i -> i).toArray();
     }
-    
+
+    // 큐 초기화
     private void init(int[] progresses, int[] speeds) {
         int taskCount = progresses.length;
-        
+
         for (int i = 0; i < taskCount; i++) {
             queue.offer(new Task(progresses[i], speeds[i]));
         }
     }
-    
+
+    // 배포 가능한지 확인
     private boolean isDeployable(int progress, int speed, int passedDays) {
         return progress + (speed * passedDays) >= 100;
     }
-    
+
+    // 배포가 가능하려면 며칠이 지나야 하는지 계산
     private int calculatePassedDays(int progress, int speed) {
         return (int) Math.ceil((double) (100 - progress) / speed);
     }
-    
+
+    // 배포
     private void deploy(int deploymentOrder) {
         if (deploymentOrder < answer.size()) {
             answer.set(deploymentOrder, answer.get(deploymentOrder) + 1);
