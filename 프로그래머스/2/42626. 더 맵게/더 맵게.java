3fa6a34e43e3ff13bf0cab5fd8ab15f279a310f9
JisuKim <jisukim.tech@gmail.com>
2025-01-12 12:46:29 +0900

    [level 2] Title: 더 맵게, Time: 1592.57 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,30 +1,36 @@
 import java.util.PriorityQueue;
 
 class Solution {
+    private static final PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    
     public int solution(int[] scovilles, int K) {
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
-        int mixCount = 0; // 섞은 횟수
+        int answer = 0;
         
-        for (int scoville : scovilles) {
-            priorityQueue.offer(scoville);
-        }
+        init(scovilles);
         
-        while (priorityQueue.size() >= 2) {
-            int minScoville1 = priorityQueue.poll(); // 가장 맵지 않은 음식의 스코빌 지수
-            int minScoville2 = priorityQueue.poll(); // 두 번째로 맵지 않은 음식의 스코빌 지수
-            
-            if (minScoville1 >= K) {
-                return mixCount;
+        while (minHeap.size() > 1) {
+            if (minHeap.peek() >= K) {
+                return answer;
             }
             
-            priorityQueue.offer(minScoville1 + 2 * minScoville2); // 가장 맵지 않은 두 음식 혼합
-            mixCount++; // 섞은 횟수 갱신
+            int minScoville1 = minHeap.poll();
+            int minScoville2 = minHeap.poll();
+
+            int mixedScoville = minScoville1 + 2 * minScoville2;
+            minHeap.offer(mixedScoville);
+            answer++;
         }
         
-        if (priorityQueue.poll() >= K) {
-            return mixCount;
-        } else {
-            return -1;
+        if (minHeap.peek() >= K) {
+            return answer;
+        }
+        
+        return -1;
+    }
+    
+    private void init(int[] scovilles) {
+        for (int scoville : scovilles) {
+            minHeap.offer(scoville);
         }
     }
 }
