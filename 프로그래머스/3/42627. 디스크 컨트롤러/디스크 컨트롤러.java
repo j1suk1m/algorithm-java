3a4346bdc7effb5e619988139291e3f579fb8f0f
JisuKim <jisukim.tech@gmail.com>
2025-01-17 23:44:43 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 6.35 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+class Solution {   
+    public int solution(int[][] jobs) {
+        PriorityQueue<int[]> heap = new PriorityQueue<>((job1, job2) -> job1[1] - job2[1]);
+        int currentTime = 0;
+        int sumOfTurnaroundTime = 0;
+        int finishedJobCount = 0; // 완료된 작업의 개수
+        int previousStartTime = -1;
+        
+        // 요청 시간이 빠른 순서대로 정렬
+        Arrays.sort(jobs, (job1, job2) -> job1[0] - job2[0]);
+        
+        // 전체 작업을 수행할 동안
+        while (finishedJobCount < jobs.length) {
+            
+            // 현재 시점에 수행할 수 있는 작업 힙에 추가
+            for (int[] job : jobs) {
+                if (previousStartTime < job[0] && job[0] <= currentTime) {
+                    heap.offer(job);
+                }
+            }
+            
+            if (heap.isEmpty()) {
+                currentTime++;
+            } else {
+                int[] job = heap.poll();
+                previousStartTime = currentTime;
+                currentTime += job[1];
+                sumOfTurnaroundTime += (currentTime - job[0]);
+                finishedJobCount++;
+            }
+        }
+        
+        return (int) sumOfTurnaroundTime / jobs.length;
+    }
+}
