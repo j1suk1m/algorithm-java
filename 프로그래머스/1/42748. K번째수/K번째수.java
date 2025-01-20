0270aa9026a78b88fcd2c12d727eba92ab9adbfe
JisuKim <jisukim.tech@gmail.com>
2025-01-20 15:12:29 +0900

    [level 1] Title: K번째수, Time: 0.49 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -8,12 +8,12 @@ class Solution {
         for (int[] command: commands) {
             int from = command[0];
             int to = command[1];
-            int target = command[2];
+            int K = command[2];
             
             int[] copiedArray = Arrays.copyOfRange(array, from - 1, to);
             Arrays.sort(copiedArray);
             
-            answer[answerIndex++] = copiedArray[target - 1];
+            answer[answerIndex++] = copiedArray[K - 1];
         }
 
         return answer;
