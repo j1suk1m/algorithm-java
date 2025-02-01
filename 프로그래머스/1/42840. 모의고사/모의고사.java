7591a4512b223230a5bf767384c0c5f6006fc407
Jisu Kim <jisukim.tech@gmail.com>
2025-02-01 21:19:08 +0900

    [level 1] Title: 모의고사, Time: 11.71 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,41 +1,74 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+
+class Student {
+    private final int id; // 번호
+    private final int[] responses; // 풀이한 답변
+    private int score = 0; // 점수
+    
+    public Student(int id, int[] responses) {
+        this.id = id;
+        this.responses = responses;
+    }
+    
+    // 한 문제 채점
+    public void calculateScore(int problemNumber, int answer) {
+        if (responses[problemNumber % responses.length] == answer) {
+            score++;
+        }
+    } 
+    
+    public int getId() {
+        return id;
+    }
+    
+    public int getScore() {
+        return score;
+    }
+}
 
 class Solution {
+    List<Integer> answer = new ArrayList<>();
+    List<Student> students = new ArrayList<>();
+
     public int[] solution(int[] answers) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        int[] score = new int[3];
-        int[] number1 = {1, 2, 3, 4, 5};
-        int[] number2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] number3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int index = 0;
-        int highest = -1;
+        initStudents(); // 학생 리스트 초기화
         
-        while (index < answers.length) {
-            if (number1[index % 5] == answers[index]) {
-                score[0]++;
-            }
-            
-            if (number2[index % 8] == answers[index]) {
-                score[1]++;
-            }
-            
-            if (number3[index % 10] == answers[index]) {
-                score[2]++;
-            }
-            
-            index += 1;
-        }
-        
-        for (int i = 0; i < 3; i++) {
-            if (highest < score[i]) {
-                highest = score[i];
-                answer.clear();
-                answer.add(i + 1);
-            } else if (highest == score[i]) {
-                answer.add(i + 1);
+        for (int problemNumber = 0; problemNumber < answers.length; problemNumber++) {
+            for (Student student : students) {
+                student.calculateScore(problemNumber, answers[problemNumber]); // 채점
             }
         }
         
-        return answer.stream().mapToInt(num -> num).toArray();
+        findStudentsWithTopScore(); // 가장 많은 문제를 맞힌 학생 조회
+        
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+    
+    // 학생 리스트 초기화
+    private void initStudents() {
+        students.add(new Student(1, new int[] {1, 2, 3, 4, 5}));
+        students.add(new Student(2, new int[] {2, 1, 2, 3, 2, 4, 2, 5}));
+        students.add(new Student(3, new int[] {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}));
+    }
+    
+    // 가장 높은 점수 조회
+    private int findTopScore() {
+        int topScore = students.stream()
+                               .mapToInt(Student::getScore)
+                               .max()
+                               .orElse(0); // 최댓값이 없을 경우 사용할 기본값
+        
+        return topScore;
+    }
+    
+    // 가장 많은 문제를 맞힌 학생 조회
+    private void findStudentsWithTopScore() {
+        int topScore = findTopScore();
+        
+        students.stream()
+                .filter(student -> student.getScore() == topScore)
+                .map(Student::getId)
+                .forEach(answer::add); // answer 리스트에 추가
     }
 }
