5d86215e69e2510d54003778c9eefb9df5b4dfb7
Jisu Kim <jisukim.tech@gmail.com>
2025-02-09 22:29:48 +0900

    [Silver III] Title: N과 M (1), Time: 232 ms, Memory: 22552 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,43 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 class Main {
-    private static final List<Integer> permutation = new ArrayList<>();
+    private static int[] permutation;
     private static boolean[] visited;
+    private static final StringBuilder answer = new StringBuilder();
+    private static int N;
+    private static int M;
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
 
-        int N = Integer.parseInt(tokenizer.nextToken());
-        int M = Integer.parseInt(tokenizer.nextToken());
+        N = Integer.parseInt(tokenizer.nextToken());
+        M = Integer.parseInt(tokenizer.nextToken());
 
+        permutation = new int[M];
         visited = new boolean[N + 1];
 
-        backtrack(N, M);
+        // 백트래킹
+        backtrack(0);
+        
+        System.out.println(answer);
     }
 
-    private static void backtrack(int N, int M) {
+    private static void backtrack(int depth) {
         
         // 수열의 길이가 M이 된 경우
-        if (permutation.size() == M) {
-            permutation.forEach(number -> System.out.print(number + " "));
-            System.out.println();
+        if (depth == M) {
+            for (int number : permutation) {
+                answer.append(number).append(" ");
+            }
+            
+            answer.append("\n");
             return;
         }
 
         for (int number = 1; number <= N; number++) {
             if (!visited[number]) {
                 visited[number] = true;
-                permutation.add(number);
-                backtrack(N, M);
-                permutation.remove(permutation.size() - 1);
+                permutation[depth] = number;
+                backtrack(depth + 1);
                 visited[number] = false;
             }
         }
+        
     }
 }
