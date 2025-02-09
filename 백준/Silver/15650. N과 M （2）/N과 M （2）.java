5bdb8b2f834fd831e01e0a734bd0de8fc7955eb0
Jisu Kim <jisukim.tech@gmail.com>
2025-02-09 22:42:03 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    private static int[] permutation;
+    private static boolean[] visited;
+    private static final StringBuilder answer = new StringBuilder();
+    private static int N;
+    private static int M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+
+        N = Integer.parseInt(tokenizer.nextToken());
+        M = Integer.parseInt(tokenizer.nextToken());
+
+        permutation = new int[M];
+        visited = new boolean[N + 1];
+
+        // 백트래킹
+        backtrack(0, 0);
+
+        System.out.println(answer);
+    }
+
+    private static void backtrack(int depth, int previousNumber) {
+
+        // 수열의 길이가 M이 된 경우
+        if (depth == M) {
+            for (int number : permutation) {
+                answer.append(number).append(" ");
+            }
+
+            answer.append("\n");
+            return;
+        }
+
+        for (int number = 1; number <= N; number++) {
+            if (!visited[number] && number > previousNumber) {
+                visited[number] = true;
+                permutation[depth] = number;
+                backtrack(depth + 1, number);
+                visited[number] = false;
+            }
+        }
+
+    }
+}
