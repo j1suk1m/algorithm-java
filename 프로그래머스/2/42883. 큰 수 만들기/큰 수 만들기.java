191bd20f75b080a12f33472ff24373c4face2944
Jisu Kim <jisukim.tech@gmail.com>
2025-02-28 23:01:38 +0900

    [level 2] Title: 큰 수 만들기, Time: 87.47 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -3,10 +3,9 @@ import java.util.Stack;
 class Solution {
     public String solution(String number, int k) {
         Stack<Integer> stack = new Stack<>();
-        int answerLength = number.length() - k;
         
         for (int i = 0; i < number.length(); i++) {
-            int currentNumber = number.charAt(i) - '0'; // char -> int
+            int currentNumber = Character.getNumericValue(number.charAt(i));
             
             if (stack.isEmpty() || (stack.peek() >= currentNumber)) {
                 stack.push(currentNumber);
@@ -20,12 +19,17 @@ class Solution {
             }
         }
         
+        while (k > 0) {
+            stack.pop();
+            k--;
+        }
+        
         StringBuilder answer = new StringBuilder();
         
         while (!stack.isEmpty()) {
             answer.append(stack.pop());
         }
         
-        return answer.reverse().substring(0, answerLength);
+        return answer.reverse().toString();
     }
 }
