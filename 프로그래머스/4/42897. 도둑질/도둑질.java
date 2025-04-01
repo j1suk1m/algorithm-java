34e44c966b7596502666a7eafe9e7db5ac4ccda7
Jisu Kim <jisukim.tech@gmail.com>
2025-04-01 23:32:33 +0900

    [level 4] Title: 도둑질, Time: 38.63 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,26 +1,43 @@
 class Solution {
+    
+    // 첫번째 행은 마지막 집을 제외하고, 두번째 행은 첫번째 집을 제외한 경우를 나타낸다.
+    // 각 행에 대하여, 열에 해당하는 집에 도착한 시점에 얻을 수 있는 최대 이익을 저장한다.
+    private int[][] table;
+    
     public int solution(int[] money) {
-        
-        // 집의 개수를 저장한다.
         int houseCount = money.length;
         
-        // 인덱스 i에 대해, i번째 집에 도착했을 때 얻을 수 있는 돈의 최댓값을 저장한다.
-        int[] table1 = new int[houseCount]; // 인덱스 범위가 0 ~ (houseCount - 2)인 집에 대해서 계산한다.
-        int[] table2 = new int[houseCount]; // 인덱스 범위가 1 ~ (houseCount - 1)인 집에 대해서 계산한다.
+        // 테이블을 초기화한다.
+        initTable(money, houseCount);
         
         // 기저 조건을 명시한다.
-        table1[0] = money[0]; // 첫번째 집에서 도둑질을 한다고 가정한다.
-        table1[1] = money[0]; // 두번째 집에서는 도둑질을 하지 못한다.
-        table2[0] = 0; // 첫번째 집에서 도둑질을 하지 않는다고 가정한다.
-        table2[1] = money[1];
+        table[0][0] = money[0];
+        table[0][1] = money[0];
+        table[1][0] = 0;
+        table[1][1] = money[1];
         
-        // 다이나믹 프로그래밍을 통해 얻을 수 있는 돈의 최댓값을 계산한다.
-        for (int i = 2; i < houseCount; i++) {
-            table1[i] = Math.max(table1[i - 2] + money[i], table1[i - 1]);
-            table2[i] = Math.max(table2[i - 2] + money[i], table2[i - 1]);    
+        // 다이나믹 프로그래밍을 실행하며 테이블을 채운다.
+        for (int i = 2; i < houseCount - 1; i++) {
+            table[0][i] = Math.max(table[0][i - 1], table[0][i - 2] + money[i]);
         }
         
-        return Math.max(table1[houseCount - 2], table2[houseCount - 1]);
+        for (int i = 2; i < houseCount; i++) {
+            table[1][i] = Math.max(table[1][i - 1], table[1][i - 2] + money[i]);
+        }
         
+        return Math.max(table[0][houseCount - 2], table[1][houseCount - 1]);
     }
+    
+    // 테이블을 초기화한다.
+    private void initTable(int[] money, int houseCount) {
+        table = new int[2][houseCount];
+        
+        for (int i = 0; i < houseCount; i++) {
+            int currentMoney = money[i];
+            
+            table[0][i] = currentMoney;
+            table[1][i] = currentMoney;
+        }
+    }
+    
 }
