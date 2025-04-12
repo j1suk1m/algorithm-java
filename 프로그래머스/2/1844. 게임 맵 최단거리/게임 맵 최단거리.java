7b97e2cd2120ca8d20b6da1e359f5672e786a18e
Jisu Kim <jisukim.tech@gmail.com>
2025-04-12 23:50:01 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.09 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,11 +1,12 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
-class Pair {
-    public int x;
-    public int y;
-    public int distance;
-
-    Pair(int x, int y, int distance) {
+class Node {
+    public int x; // 행 // 0부터 시작
+    public int y; // 열 // 0부터 시작
+    public int distance; // 지나온 칸의 개수 // 1부터 시작
+    
+    public Node(int x, int y, int distance) {
         this.x = x;
         this.y = y;
         this.distance = distance;
@@ -13,50 +14,51 @@ class Pair {
 }
 
 class Solution {
-    private static int n;
-    private static int m;
-    private static int wall = 0;
-    private static int[] dx = {-1, 1, 0, 0};
-    private static int[] dy = {0, 0, -1, 1};
-    private static Queue<Pair> queue = new LinkedList<>();
-
-    private static int bfs(int[][] maps) {
+    private static final int WALL = 0; // 벽이 있는 자리
+    private static final int SPACE = 1; // 벽이 없는 자리
+    private static final int DIRECTION_COUNT = 4; // 이동 가능한 방향 개수
+    private final int[] dx = {-1, 1, 0, 0}; // 상, 하, 좌, 우
+    private final int[] dy = {0, 0, -1, 1}; // 상, 하, 좌, 우
+    private final Queue<Node> queue = new LinkedList<>();
+    
+    public int solution(int[][] maps) {
+        int startX = 0;
+        int startY = 0;
+        int endX = maps.length - 1;
+        int endY = maps[0].length - 1;
+        
+        return bfs(maps, startX, startY, endX, endY);
+    }
+    
+    private int bfs(int[][] maps, int startX, int startY, int endX, int endY) {
         int answer = -1;
-        maps[0][0] = wall;
-        queue.add(new Pair(0, 0, 1));
-
+        
+        maps[startX][startY] = WALL; // 방문 표시 // 벽이 있는 자리로 변경
+        queue.add(new Node(startX, startY, 1)); // 큐에 추가
+        
         while (!queue.isEmpty()) {
-            Pair pair = queue.poll();
-
-            // 상대 팀 진영에 도착
-            if (pair.x == n - 1 && pair.y == m - 1) {
-                answer = pair.distance;
+            Node current = queue.poll();
+            
+            if (current.x == endX && current.y == endY) { // 종료 조건
+                answer = current.distance;
                 break;
             }
-
-            // 상하좌우 탐색
-            for (int i = 0; i < 4; i++) {
-                int nx = pair.x + dx[i];
-                int ny = pair.y + dy[i];
-
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+            
+            for (int i = 0; i < DIRECTION_COUNT; i++) { // 상하좌우 탐색
+                int nx = current.x + dx[i];
+                int ny = current.y + dy[i];
+                
+                if (nx < 0 || nx > endX || ny < 0 || ny > endY) { // 범위를 벗어나는 경우
                     continue;
-                } else if (maps[nx][ny] == wall) {
+                } else if (maps[nx][ny] == WALL) { // 벽이 있는 자리인 경우
                     continue;
-                } else { // 맵을 벗어나지 않고 벽이 아닌 경우
-                    maps[nx][ny] = wall;
-                    queue.add(new Pair(nx, ny, pair.distance + 1));
+                } else {
+                    maps[nx][ny] = WALL; // 방문 표시 // 벽이 있는 자리로 변경
+                    queue.add(new Node(nx, ny, current.distance + 1)); // 큐에 추가
                 }
             }
         }
         
         return answer;
-    }
-
-    public int solution(int[][] maps) {
-        n = maps.length;
-        m = maps[0].length;
-
-        return bfs(maps);
     }
 }
