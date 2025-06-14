c7d353065afaa2c475e48c2dfeccfe9e247f3bba
Jisu Kim <jisukim.tech@gmail.com>
2025-06-15 00:16:19 +0900

    [level 3] Title: 입국심사, Time: 121.84 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,15 +1,13 @@
 import java.util.Arrays;
 
 class Solution {
-    public long solution(int n, int[] times) {
-        long answer = 0;
-        
+    public long solution(int n, int[] times) {        
         Arrays.sort(times); // 이분 탐색을 위한 오름차순 정렬
         
-        long left = 1; // 최소 시간
+        long left = 0; 
         long right = (long) n * times[times.length - 1]; // 최대 시간
         
-        while (left <= right) {
+        while (left + 1 < right) {
             long mid = (left + right) / 2;
             long totalCount = 0; // mid분 내에 심사할 수 있는 사람의 수
             
@@ -18,13 +16,12 @@ class Solution {
             }
             
             if (totalCount >= n) { // 충분히 심사할 수 있으면, 더 짧은 시간도 가능한지 탐색
-                answer = mid;
-                right = mid - 1; 
+                right = mid; 
             } else { // 시간이 부족한 경우 더 긴 시간으로 탐색
-                left = mid + 1;
+                left = mid;
             }
         }
         
-        return answer;
+        return right;
     }
 }
