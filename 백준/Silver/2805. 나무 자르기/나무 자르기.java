2fe221e14fa633b165f821d9522c3906f2cf80d0
Jisu Kim <jisukim.tech@gmail.com>
2025-07-29 23:59:59 +0900

    [Silver II] Title: 나무 자르기, Time: 908 ms, Memory: 119276 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,55 +1,58 @@
 import java.util.*;
 import java.io.*;
 
-class Main {
-    private static int[] heights;
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokenizer = new StringTokenizer(reader.readLine(), " ");
+public class Main {
+	static int[] treeHeights; // 나무 높이 배열
 
-        int treeCount = Integer.parseInt(tokenizer.nextToken()); 
-        int targetHeight = Integer.parseInt(tokenizer.nextToken());  
-        
-        heights = new int[treeCount];
-        tokenizer = new StringTokenizer(reader.readLine(), " ");
-        
-        for (int i = 0; i < treeCount; i++) {
-            heights[i] = Integer.parseInt(tokenizer.nextToken()); 
-        }
-
-        Arrays.sort(heights);
-
-        // check(low) != check(high)가 되도록 low, high의 초깃값 설정
-        int low = 0; // check(low) == true
-        int high = heights[treeCount - 1]; // check(high) == false
-
-        while (low + 1 < high) {
-            int mid = (low + high) / 2; // 절단기 높이
-
-            if (check(treeCount, targetHeight, mid)) { // 적어도 ${targetHeight}미터를 가져갈 수 있는 경우
-                low = mid; // 절단기 높이를 높여도 ${targetHeight}미터 이상을 가져갈 수 있는지 확인
-            } else {
-                high = mid; // 절단기 높이를 낮춰 나무를 더 많이 자름
-            }
-        }
-
-        System.out.println(low);
-    }
-
-    private static boolean check(int treeCount, int targetHeight, int mid) {
-        long totalHeight = 0L; // 덧셈 과정에서 오버플로우가 발생할 수 있으므로 int가 아닌 long 사용
-
-        for (int i = treeCount - 1; i >= 0; i--) {
-            int height = heights[i];
-
-            if (mid >= height) { // 조기 종료 조건
-                break;
-            }
-
-            totalHeight += (long) height - mid;
-        }
-
-        return totalHeight >= (long) targetHeight;
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int treeCount = Integer.parseInt(st.nextToken()); // 나무의 수
+		long targetHeight = Long.parseLong(st.nextToken()); // 집으로 가져가려고 하는 나무의 길이
+		
+		treeHeights = new int[treeCount];
+		st = new StringTokenizer(br.readLine(), " ");
+		
+		for (int i = 0; i < treeCount; i++) { // 나무 높이 배열 저장
+			treeHeights[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(treeHeights); // 매개변수 탐색을 위한 오름차순 정렬
+		
+		System.out.println(runParametricSearch(targetHeight));
+	}
+	
+	// 매개변수 탐색을 이용한 최적의 절단기 높이 계산
+	static int runParametricSearch(long targetHeight) {
+		int left = 0; // check(left) == true // 나무를 targetHeight만큼 무조건 가져갈 수 있음
+		int right = treeHeights[treeHeights.length - 1]; // check(right) == false // 나무를 하나도 가져가지 못함
+		
+		while (left + 1 < right) {
+			int mid = left - (left - right) / 2; // (left + right) / 2와 동일
+			
+			if (check(mid, targetHeight)) { // 절단기 높이가 mid일 때, 적어도 targetHeight만큼 나무를 가져갈 수 있음
+				left = mid; // 절단기 높이를 더 높여도 targetHeight만큼 가져갈 수 있는지 탐색
+			} else { // 절단기 높이가 mid일 때, targetHeight만큼 나무를 가져가지 못함
+				right = mid; // 절단기 높이를 낮춰서 나무를 더 많이 잘라봄
+			}
+		}
+		
+		return left;
+	}
+	
+	// 절단기 높이가 mid일 때, 적어도 targetHeight만큼 나무를 가져갈 수 있는지 확인
+	static boolean check(int mid, long targetHeight) {
+		long sum = 0L; // 나무의 수가 최대 1e6, 나무의 높이가 최대 1e9이므로 오버플로 방지를 위해 long 사용
+		
+		for (int i = treeHeights.length - 1; i >= 0; i--) { // 절단기로 자른 나무 길이의 합 계산
+			int treeHeight = treeHeights[i];
+			
+			if (treeHeight <= mid) break;
+			
+			sum += (long) (treeHeights[i] - mid);
+		}
+		
+		return sum >= targetHeight;
+	}
 }
