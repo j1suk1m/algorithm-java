1ff3939b396a928d7284574234f470deadaac533
Jisu Kim <jisukim.tech@gmail.com>
2025-09-10 20:56:54 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.06 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -1,50 +1,33 @@
 class Solution {
-    static final int RIGHT = 0;
-    static final int BOTTOM = 1;
-    static final int LEFT = 2;
-    static final int TOP = 3;
-    
     public int[][] solution(int n) {
         int[][] answer = new int[n][n];
+        int start = 0;
+        int end = n - 1;
         int number = 1;
-        int direction = RIGHT;
-        int x = 0;
-        int y = 0;
         
         while (number <= n * n) {
-            answer[x][y] = number;
-            
-            if (direction == RIGHT) {
-                if (y == n - 1 || answer[x][y + 1] != 0) { // 방향 변환 필요
-                    direction = BOTTOM;
-                    x++;
-                } else {
-                    y++;
-                }               
-            } else if (direction == BOTTOM) {
-                if (x == n - 1 || answer[x + 1][y] != 0) {
-                    direction = LEFT;
-                    y--;
-                } else {
-                    x++;
-                }
-            } else if (direction == LEFT) {
-                if (y == 0 || answer[x][y - 1] != 0) {
-                    direction = TOP;
-                    x--;
-                } else {
-                    y--;
-                }
-            } else {
-                if (x == 0 || answer[x - 1][y] != 0) {
-                    direction = RIGHT;
-                    y++;
-                } else {
-                    x--;
-                }
+            // 오른쪽 방향 채우기
+            for (int y = start; y <= end; y++) {
+                answer[start][y] = number++;
             }
             
-            number++;
+            // 아래쪽 방향 채우기
+            for (int x = start + 1; x <= end; x++) {
+                answer[x][end] = number++;
+            }
+            
+            // 왼쪽 방향 채우기
+            for (int y = end - 1; y >= start; y--) {
+                answer[end][y] = number++;
+            }
+            
+            // 위쪽 방향 채우기
+            for (int x = end - 1; x > start; x--) {
+                answer[x][start] = number++;
+            }
+            
+            start++;
+            end--;
         }
         
         return answer;
